011673d21fc6938b9a918982f2b45724305faa04
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    kuntao: Build a dummy android.hidl.base@1.0 for Oreo blobs
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- device.mk
+++ device.mk
@@ -162,6 +162,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0-java
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
